41d429efea1e229725c31089472d81e60ed5ac4d
oussamasine07 <sineoussama@gmail.com>
2025-05-20 17:49:00 +0100

    create event model

---
+++ backend/src/main/java/com/helloevent/backend/model/Event.java
@@ -0,0 +1,70 @@
+package com.helloevent.backend.model;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "events")
+public class Event {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "name", nullable = false)
+    private String name;
+
+    @Column(name = "description", nullable = false)
+    private String description;
+
+    @Column(name = "place", nullable = false)
+    private String place;
+
+    @Column(name = "number_of_places", nullable = false)
+    private int numberOfPlaces;
+
+    /*
+    * TODO
+    *       - add status enum(opened, closed)
+    *       - add relationships
+    * */
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getPlace() {
+        return place;
+    }
+
+    public void setPlace(String place) {
+        this.place = place;
+    }
+
+    public int getNumberOfPlaces() {
+        return numberOfPlaces;
+    }
+
+    public void setNumberOfPlaces(int numberOfPlaces) {
+        this.numberOfPlaces = numberOfPlaces;
+    }
+}
--- backend/src/main/java/com/helloevent/backend/model/User.java
+++ backend/src/main/java/com/helloevent/backend/model/User.java
@@ -24,6 +24,8 @@ public class User {
     @Column(name = "password", nullable = false)
     private String password;
 
+    // todo add role
+
 
     // todo: make relationships here
 
