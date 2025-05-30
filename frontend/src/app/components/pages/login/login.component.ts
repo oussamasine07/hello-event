7b3b8c71b5799831e3afe3d9424962d411b54bd5
oussamasine07 <sineoussama@gmail.com>
2025-05-30 11:26:00 +0100

    add login in the ui

--- frontend/src/app/components/pages/login/login.component.ts
+++ frontend/src/app/components/pages/login/login.component.ts
@@ -1,9 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import {MatCardModule} from '@angular/material/card';
 import {MatFormFieldModule} from '@angular/material/form-field';
 import {MatInputModule} from '@angular/material/input';
 import {MatButtonModule} from '@angular/material/button';
 import {MatIconModule} from '@angular/material/icon';
+import {FormsModule} from '@angular/forms';
+import {AuthService} from '../../../services/auth/auth.service';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -12,11 +15,39 @@ import {MatIconModule} from '@angular/material/icon';
     MatFormFieldModule,
     MatInputModule,
     MatButtonModule,
-    MatIconModule
+    MatIconModule,
+    FormsModule
   ],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
   hidePassword = true;
+
+  router = inject(Router);
+
+  authService = inject(AuthService)
+
+  loginFormObj = {
+    username: "",
+    password: ""
+  }
+
+  onLoginFormSubmit (form: FormsModule) {
+
+    this.authService.loginClient(this.loginFormObj).subscribe({
+      next: (res: string) => {
+        localStorage.setItem("token", res)
+      }
+    })
+
+    this.loginFormObj = {
+      username: "",
+      password: ""
+    }
+
+    this.router.navigate(["/dashboard"])
+
+  }
+
 }
--- frontend/src/app/services/auth/auth.service.ts
+++ frontend/src/app/services/auth/auth.service.ts
@@ -1,11 +1,17 @@
 import { Injectable, inject } from '@angular/core';
 import {HttpClient} from '@angular/common/http';
+import {Observable} from 'rxjs';
 
 type registerBody = {
   first_name: string,
   last_name: string,
   username: string,
   email: string,
+  password: string
+}
+
+type loginBody = {
+  username: string,
   password: string
 }
 
@@ -20,6 +26,11 @@ export class AuthService {
 
   registerClient (body: registerBody) {
     return this.httpClient.post("http://localhost:8080/user/register", body);
+  }
+
+  loginClient ( body: loginBody ): Observable<string> {
+    console.log(body)
+    return this.httpClient.post("http://localhost:8080/user/login", body, { responseType: 'text'});
   }
 
 }
