f09050bdd92c54395c7867407476673d8a97ba20
oussamasine07 <sineoussama@gmail.com>
2025-06-02 20:46:52 +0100

    handle logout functionality on admin side

--- frontend/src/app/components/layouts/dashboard-layout/navbar/navbar.component.ts
+++ frontend/src/app/components/layouts/dashboard-layout/navbar/navbar.component.ts
@@ -1,22 +1,34 @@
-import {Component, EventEmitter, Output} from '@angular/core';
+import {Component, EventEmitter, inject, Output} from '@angular/core';
 import {MatToolbarModule} from '@angular/material/toolbar';
 import {MatButtonModule} from '@angular/material/button';
 import {MatIconModule} from '@angular/material/icon';
+import {NgIf} from '@angular/common';
+import {Router, RouterLink, RouterLinkActive} from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
   imports: [
     MatToolbarModule,
     MatButtonModule,
-    MatIconModule
+    MatIconModule,
+    NgIf,
+    RouterLink,
+    RouterLinkActive
   ],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css'
 })
 export class NavbarComponent {
+  router: Router = inject(Router)
+
   @Output() menuToggle = new EventEmitter<void>();
 
   onMenuClick() {
     this.menuToggle.emit();
+  }
+
+  onLogoutClick () {
+    localStorage.removeItem("token");
+    this.router.navigate(["/login"])
   }
 }
